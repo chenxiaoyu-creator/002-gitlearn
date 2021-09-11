9ae23dd4653ca93d23192b9bbf76b90b7329daf2
chenxiaoyu-creator <3283146539@qq.com>
2021-09-11 23:04:31 +0800

    dddd

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -10,5 +10,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("hello1");
+        System.out.println("hello3");
     }
 }
