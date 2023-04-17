54e4c1b2ff1facb6bff503b55908298434001c3a
chenxiaoyu-creator <75022072+chenxiaoyu-creator@users.noreply.github.com>
2023-04-17 11:37:20 +0800

    ceshi

--- src/test/java/com/jd/learn.java
+++ src/test/java/com/jd/learn.java
@@ -3,6 +3,8 @@ package com.jd;
 
 public class learn {
     public static void main(String[] args) {
+        int a = 12;
+        System.out.println(a);
         System.out.println(12233);
         System.out.println(122);
         System.out.println(1225);
